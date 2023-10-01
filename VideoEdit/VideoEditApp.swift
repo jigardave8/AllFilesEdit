df0b24ad9108cce60a43a5344f5f28afb868c8ca
Jay D <jdavenz8@gmail.com>
2023-10-02 12:05:54 +1300

    Initial Commit

---
+++ VideoEdit/VideoEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditApp.swift
+//  VideoEdit
+//
+//  Created by Jigar on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
