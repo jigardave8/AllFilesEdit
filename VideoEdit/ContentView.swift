886095c85c0cfa8611252ce800092ad0298a2cf1
Jay D <jdavenz8@gmail.com>
2023-10-02 12:52:53 +1300

    files selecing and showing in viewer

--- VideoEdit/ContentView.swift
+++ VideoEdit/ContentView.swift
@@ -6,19 +6,90 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
+import QuickLook
 
 struct ContentView: View {
+    @State private var selectedURL: URL?
+    @State private var isImagePickerPresented: Bool = false
+    @State private var isDocumentPickerPresented: Bool = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let url = selectedURL {
+                FileViewer(url: url)
+            } else {
+                Button("Select Photo or File") {
+                    isImagePickerPresented.toggle()
+                }
+                .padding()
+                .fileImporter(
+                    isPresented: $isDocumentPickerPresented,
+                    allowedContentTypes: [UTType.data],
+                    onCompletion: { result in
+                        do {
+                            selectedURL = try result.get() as? URL
+                        } catch {
+                            print("Error importing document: \(error)")
+                        }
+                    }
+                )
+
+                .onChange(of: isImagePickerPresented) { newValue in
+                    if newValue {
+                        isDocumentPickerPresented = true
+                    }
+                }
+            }
         }
-        .padding()
+        .navigationTitle("File Viewer")
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct FileViewer: View {
+    let url: URL
+
+    var body: some View {
+        QuickLookView(url: url)
+    }
+}
+
+struct QuickLookView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<QuickLookView>) -> QLPreviewController {
+        let controller = QLPreviewController()
+        controller.dataSource = context.coordinator
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: QLPreviewController, context: UIViewControllerRepresentableContext<QuickLookView>) {
+        // Update the view controller
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, QLPreviewControllerDataSource {
+        var parent: QuickLookView
+
+        init(_ parent: QuickLookView) {
+            self.parent = parent
+        }
+
+        func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+            return 1
+        }
+
+        func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+            return parent.url as QLPreviewItem
+        }
+    }
 }
